3121eb2e1fec45534d13011452f96e54c1801095
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-04-26 23:52:47 +0900

    ゲームループ

--- main.cpp
+++ main.cpp
@@ -534,27 +534,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	scissoRect.top = 0;
 	scissoRect.bottom = kClientHeight;
 
-	commandList->RSSetViewports(1, &viewport);  //viewportを設定
-	commandList->RSSetScissorRects(1, &scissoRect);    //Scirssorを設定
-	// RootSignatureを設定。PSOに設定しているけど別途設定が必要
-	commandList->SetGraphicsRootSignature(rootSignature);
-	commandList->SetPipelineState(graphicsPipelineState);    //PSOを設定
-	commandList->IASetVertexBuffers(0, 1, &vertexBufferView);    //VBVを設定
-	//形状を設定。PSOに設定しているものとはまた別。同じものを設定すると考えておけば良い
-	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	// 描画！（DrawCall/ドローコール）・3頂点で1つのインスタンス。インスタンスについては今後
-	commandList->DrawInstanced(3, 1, 0, 0);
-
-
-
-
-
-
-
-
-
-		
-
+	
 
 	MSG msg{};
 	//ウィンドウのxボタンが押されるまでループ
@@ -592,6 +572,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			//指定した色で画面全体をクリアする
 			float clearColor[] = { 0.1f,0.25f,0.5f,1.0f }; //青っぽい色。 RGBAの淳  0.1/0.25/0.5/1.0f
 			commandList->ClearRenderTargetView(rtvHandles[backBufferIndex], clearColor, 0, nullptr);
+
+			commandList->RSSetViewports(1, &viewport);  //viewportを設定
+			commandList->RSSetScissorRects(1, &scissoRect);    //Scirssorを設定
+			// RootSignatureを設定。PSOに設定しているけど別途設定が必要
+			commandList->SetGraphicsRootSignature(rootSignature);
+			commandList->SetPipelineState(graphicsPipelineState);    //PSOを設定
+			commandList->IASetVertexBuffers(0, 1, &vertexBufferView);    //VBVを設定
+			//形状を設定。PSOに設定しているものとはまた別。同じものを設定すると考えておけば良い
+			commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+			// 描画！（DrawCall/ドローコール）・3頂点で1つのインスタンス。インスタンスについては今後
+			commandList->DrawInstanced(3, 1, 0, 0);
+
 
 			//画面に描く処理はすべて終わり、画面に移すので、状態を遷移
 	// 今回はRenderTargetからPresentにする
